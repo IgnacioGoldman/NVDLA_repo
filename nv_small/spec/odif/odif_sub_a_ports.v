cad1b4844a1172f16ce5887b697b1f6f4749efbc
IgnacioGoldman <ignig22@gmail.com>
2018-05-29 00:38:24 +0200

    nv_small

---
+++ nv_small/spec/odif/odif_sub_a_ports.v
@@ -0,0 +1,5 @@
+,output a2b_req_valid
+,input a2b_req_ready
+,output [11:0] a2b_req_addr
+,output  a2b_req_write
+,output [31:0] a2b_req_data
---
+++ nv_small/spec/odif/odif_sub_b_ports.v
@@ -0,0 +1,5 @@
+,input a2b_req_valid
+,output a2b_req_ready
+,input [11:0] a2b_req_addr
+,input  a2b_req_write
+,input [31:0] a2b_req_data
